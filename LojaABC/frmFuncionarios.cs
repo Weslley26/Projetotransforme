17862379046a47e6fdfef16305e5cb3f2f4fb005
weslley26 <Wesley.sjesus7@senacsp.com.br>
2025-04-07 14:37:37 -0300

    first commit

---
+++ LojaABC/frmFuncionarios.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+//importando a bliblioteca do banco de dados
+using MySql.Data.MySqlClient;
+
+namespace LojaABC
+{
+    public partial class frmFuncionarios : Form
+    {
+        //Criando variáveis para controle do menu
+        const int MF_BYCOMMAND = 0X400;
+        [DllImport("user32")]
+        static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("user32")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32")]
+        static extern int GetMenuItemCount(IntPtr hWnd);
+
+        public frmFuncionarios()
+        {
+            InitializeComponent();
+            //executando desabilitar os campos
+            desabilitarCampos();
+        }
+
+        public frmFuncionarios(string descricao)
+        {
+            InitializeComponent();
+            //executando desabilitar os campos
+            desabilitarCampos();
+            txtNome.Text = descricao;
+            habilitarCampos_pesquisar();
+        }
+
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            frmMenuPrincipal abrir = new frmMenuPrincipal();
+            abrir.Show();
+            this.Hide();
+        }
+
+        private void frmFuncionarios_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+            int MenuCount = GetMenuItemCount(hMenu) - 1;
+            RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
+        }
+
+        //desabilitando os campos
+        public void desabilitarCampos()
+        {
+            txtNome.Enabled = false;
+            txtEmail.Enabled = false;
+            mskCPF.Enabled = false;
+            dtpDataNascimento.Enabled = false;
+            mskCelular.Enabled = false;
+            gpbSexo.Enabled = false;
+
+            txtLogradouro.Enabled = false;
+            txtNumero.Enabled = false;
+            mskCEP.Enabled = false;
+            txtCidade.Enabled = false;
+            txtEsdado.Enabled = false;
+            cbbUF.Enabled = false;
+            txtComplemento.Enabled = false;
+
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnLimpar.Enabled = false;
+        }
+
+
+        //Habilitar os campos
+        public void habilitarCampos()
+        {
+            txtNome.Enabled = true;
+            txtEmail.Enabled = true;
+            mskCPF.Enabled = true;
+            dtpDataNascimento.Enabled = true;
+            mskCelular.Enabled = true;
+            gpbSexo.Enabled = true;
+
+            txtLogradouro.Enabled = true;
+            txtNumero.Enabled = true;
+            mskCEP.Enabled = true;
+            txtCidade.Enabled = true;
+            txtEsdado.Enabled = true;
+            cbbUF.Enabled = true;
+            txtComplemento.Enabled = true;
+
+            btnCadastrar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnLimpar.Enabled = true;
+
+            btnNovo.Enabled = false;
+
+            txtNome.Focus();
+        }
+
+        //Habilitar os campos pesquisar
+        public void habilitarCampos_pesquisar()
+        {
+            txtNome.Enabled = true;
+            txtEmail.Enabled = true;
+            mskCPF.Enabled = true;
+            dtpDataNascimento.Enabled = true;
+            mskCelular.Enabled = true;
+            gpbSexo.Enabled = true;
+
+            txtLogradouro.Enabled = true;
+            txtNumero.Enabled = true;
+            mskCEP.Enabled = true;
+            txtCidade.Enabled = true;
+            txtEsdado.Enabled = true;
+            cbbUF.Enabled = true;
+            txtComplemento.Enabled = true;
+
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnLimpar.Enabled = true;
+            btnNovo.Enabled = false;
+
+            txtNome.Focus();
+        }
+
+        //limpar os campos
+        public void limparCampos()
+        {
+            txtNome.Clear();
+            txtEmail.Clear();
+            mskCPF.Clear();
+            dtpDataNascimento.Text = "";
+            mskCelular.Clear();
+
+            rdbFeminino.Checked = false;
+            rdbMasculino.Checked = false;
+            rdbNaoDesejoInformar.Checked = false;
+
+            txtLogradouro.Clear();
+            txtNumero.Clear();
+            mskCEP.Clear();
+            txtCidade.Clear();
+            txtEsdado.Clear();
+            cbbUF.Text = "";
+            txtComplemento.Clear();
+
+            txtNome.Focus();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            //executando habilitar campos
+            habilitarCampos();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //executando limpar campos
+            limparCampos();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text.Equals("") || txtEmail.Text.Equals("") ||
+                mskCPF.Text.Equals("   .   .   -") ||
+                mskCelular.Text.Equals("     -") ||
+                txtLogradouro.Text.Equals("") ||
+                txtNumero.Text.Equals("") ||
+                txtComplemento.Text.Equals("") ||
+                txtCidade.Text.Equals("") ||
+                txtEsdado.Text.Equals("") ||
+                mskCEP.Text.Equals("     -") ||
+                cbbUF.Text.Equals(""))
+            {
+                MessageBox.Show("Favor preencher os campos!!!");
+            }
+            else
+            {
+                MessageBox.Show("Cadastrado com sucesso!!!");
+                limparCampos();
+                desabilitarCampos();
+                btnNovo.Enabled = true;
+                btnNovo.Focus();
+            }
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            frmPesquisarFuncionarios abrir = new frmPesquisarFuncionarios();
+            abrir.Show();
+            this.Hide();
+        }
+
+        public void cadastrarFuncionarios()
+        {
+                  }
+    }
+}
---
+++ LojaABC/frmPesquisarFuncionarios.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace LojaABC
+{
+    public partial class frmPesquisarFuncionarios : Form
+    {
+        //Criando variáveis para controle do menu
+        const int MF_BYCOMMAND = 0X400;
+        [DllImport("user32")]
+        static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("user32")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32")]
+        static extern int GetMenuItemCount(IntPtr hWnd);
+
+        public frmPesquisarFuncionarios()
+        {
+            InitializeComponent();
+            desabilitarCampos();
+            
+        }
+        public void desabilitarCampos()
+        {
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+           
+        }
+
+        private void frmPesquisarFuncionarios_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+            int MenuCount = GetMenuItemCount(hMenu) - 1;
+            RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //executando o método limpar campos
+            limparCampos();
+        }
+
+        //habilitar campos
+        public void habilitarCampos()
+        {
+            txtDescricao.Enabled = true;
+            btnLimpar.Enabled = true;
+            btnPesquisar.Enabled = true;
+            txtDescricao.Focus();
+        }
+
+        //criando o método limpar campos
+        public void limparCampos()
+        {
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+            ltbPesquisar.Items.Clear();
+            txtDescricao.Clear();
+            txtDescricao.Focus();
+            txtDescricao.Enabled = false;
+        }
+
+        //criando o método limpar campos pesquisar
+        public void limparCampos_pesquisar()
+        {
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+            txtDescricao.Clear();
+            txtDescricao.Focus();
+            txtDescricao.Enabled = false;
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            ltbPesquisar.Items.Clear();
+            ltbPesquisar.Items.Add(txtDescricao.Text);
+            limparCampos_pesquisar();
+           
+        }
+
+        private void rdbCodigo_CheckedChanged(object sender, EventArgs e)
+        {
+            habilitarCampos();
+        }
+
+        private void rdbNome_CheckedChanged(object sender, EventArgs e)
+        {
+            habilitarCampos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmFuncionarios abrir = new frmFuncionarios("Senac");
+            abrir.Show();
+            this.Hide();
+        }
+
+        private void ltbPesquisar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string descricao = ltbPesquisar.SelectedItem.ToString();
+            frmFuncionarios abrir = new frmFuncionarios(descricao);
+            abrir.Show();
+            this.Hide();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            frmFuncionarios abrir = new frmFuncionarios();
+            abrir.Show();
+            this.Hide();
+        }
+    }
+}
